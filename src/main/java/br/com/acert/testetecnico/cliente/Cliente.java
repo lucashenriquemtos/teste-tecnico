3766e0e6da71ea109081f3f209e69d489233071e
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-05-28 22:14:11 -0300

    Adicionando Cliente.java

---
+++ src/main/java/br/com/acert/testetecnico/cliente/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.acert.testetecnico.cliente;
+
+import br.com.acert.testetecnico.pedido.Pedido;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "Cliente")
+@Table(name = "cliente")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String email;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos;
+
+}
