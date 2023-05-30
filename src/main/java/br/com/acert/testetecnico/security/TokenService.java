d4a91f7704ddad0f280b438ac700e8ee84516371
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-05-30 16:09:42 -0300

    Adicionando AuthenticationService e TokenService

---
+++ src/main/java/br/com/acert/testetecnico/security/TokenService.java
@@ -0,0 +1,24 @@
+package br.com.acert.testetecnico.security;
+
+import br.com.acert.testetecnico.user.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Service
+public class  TokenService {
+	public String gerarToken(Usuario usuario) {
+		return JWT.create()
+				.withIssuer("Pedidos")
+				.withSubject(usuario.getUsername())
+				.withClaim("id", usuario.getId())
+				.withExpiresAt(LocalDateTime.now()
+						.plusMinutes(10)
+						.toInstant(ZoneOffset.of("-03:00")))
+				.sign(Algorithm.HMAC256("acert"));
+	}
+}
