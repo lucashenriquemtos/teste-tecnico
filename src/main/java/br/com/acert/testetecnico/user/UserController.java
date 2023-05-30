6ac425b1259b215eaeddf74c0bf5e9757f0c5b1e
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-05-30 17:14:29 -0300

    Atualizando UserController

--- src/main/java/br/com/acert/testetecnico/user/UserController.java
+++ src/main/java/br/com/acert/testetecnico/user/UserController.java
@@ -2,7 +2,11 @@ package br.com.acert.testetecnico.user;
 
 import br.com.acert.testetecnico.arch.BaseController;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/user")
 public class UserController extends BaseController<Usuario, Long> {
 	public UserController(JpaRepository<Usuario, Long> repository) {
 		super(repository);
