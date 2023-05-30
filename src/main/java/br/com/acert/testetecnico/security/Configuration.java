d5961f9a3c749283637ed03ad199b829033d4e14
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-05-30 16:09:56 -0300

    Adicionando Configuration

---
+++ src/main/java/br/com/acert/testetecnico/security/Configuration.java
@@ -0,0 +1,36 @@
+package br.com.acert.testetecnico.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@org.springframework.context.annotation.Configuration
+@EnableWebSecurity
+
+public class Configuration {
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeHttpRequests()
+				.antMatchers(HttpMethod.POST,"/login").permitAll()
+				.anyRequest().authenticated().and().build();
+	}
+}
